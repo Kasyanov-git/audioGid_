2a76a6bcf9a93b95dfe603677111248dd8bd1fbd
A1sh1teru <reserved.coliseum@gmail.com>
2025-01-23 22:37:05 +0300

    изменил кучу всяких мелочей, начал менять дизайн

---
+++ theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+    colors: {
+      primary: '#007bff',
+      secondary: '#6c757d',
+      background: '#F4F4F4',
+      text: '#2B2B2B',
+      text2: '#757575',
+      text3: 'red',
+      error: '#dc3545',
+    },
+    fonts: {
+      regular: 'Roboto-Regular',
+      bold: 'Roboto-Bold',
+    },
+  };
--- ui/screens/HomeScreen.tsx
+++ ui/screens/HomeScreen.tsx
@@ -3,8 +3,18 @@ import React, { useState } from 'react';
 import { Alert, Pressable, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View, KeyboardAvoidingView, Keyboard,
   TouchableWithoutFeedback, } from 'react-native';
 import Map from '../components/Map';
-import { Search,Suggest  } from 'react-native-yamap';
+import { Search, Suggest  } from 'react-native-yamap';
 import { GeoFigureType } from 'react-native-yamap/build/Search';
+import { theme } from '../../theme';
+import Settings from '../../assets/images/icons/settings.svg';
+import Previous from '../../assets/images/icons/previous.svg';
+import Minus15 from '../../assets/images/icons/minus15.svg';
+import Play from '../../assets/images/icons/play.svg';
+import Plus15 from '../../assets/images/icons/plus15.svg';
+import Next from '../../assets/images/icons/next.svg';
+import Like from '../../assets/images/icons/like.svg';
+import Line from '../../assets/images/icons/line.svg';
+import Pause from '../../assets/images/icons/pause.svg';
 
 function HomeScreen(): React.JSX.Element {
 
@@ -16,46 +26,46 @@ function HomeScreen(): React.JSX.Element {
   //   console.log(suggestions)
   //   Suggest.reset();
   // };
-  const handlePressTest = async () => {
-    try {
-      //Поиск по тексту(ищет как раз метки в близи)
-      const searchResult = await Search.searchText(
-        'Памятники',
-        { type: GeoFigureType.POINT, value: {lat: 59.9537667, lon: 30.4121783}},
-        { disableSpellingCorrection: true, geometry: true },
-      );
-      console.log(searchResult);
-      const response = await fetch('http://localhost:5555/ask', {  //ЗДЕСЬ МОЖНО ПОМЕНЯТЬ IP СЕРВЕРА
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+  // const handlePressTest = async () => {
+  //   try {
+  //     //Поиск по тексту(ищет как раз метки в близи)
+  //     const searchResult = await Search.searchText(
+  //       'Памятники',
+  //       { type: GeoFigureType.POINT, value: {lat: 59.9537667, lon: 30.4121783}},
+  //       { disableSpellingCorrection: true, geometry: true },
+  //     );
+  //     console.log(searchResult);
+  //     const response = await fetch('http://localhost:5555/ask', {  //ЗДЕСЬ МОЖНО ПОМЕНЯТЬ IP СЕРВЕРА
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
 
-        body: JSON.stringify({
-          searchResult,
-        }),
-      });
+  //       body: JSON.stringify({
+  //         searchResult,
+  //       }),
+  //     });
 
-      if (!response.ok) {
-        throw new Error(`HTTP Error: ${response.status}`);
-      }
+  //     if (!response.ok) {
+  //       throw new Error(`HTTP Error: ${response.status}`);
+  //     }
 
-      const data = await response.json();
-      setResponseText(data);
+  //     const data = await response.json();
+  //     setResponseText(data);
 
-      console.log('Ответ от сервера:', data);
+  //     console.log('Ответ от сервера:', data);
 
-      if (data.message) {
-        setResponseText(data.message);
-        setIsVisible(true);
-      } else {
-        throw new Error('Ответ не содержит поля "message"');
-      }
-    } catch (error) {
-      console.error('Ошибка при запросе:', error);
-      Alert.alert('Ошибка', 'Не удалось получить рассказ');
-    }
-  };
+  //     if (data.message) {
+  //       setResponseText(data.message);
+  //       setIsVisible(true);
+  //     } else {
+  //       throw new Error('Ответ не содержит поля "message"');
+  //     }
+  //   } catch (error) {
+  //     console.error('Ошибка при запросе:', error);
+  //     Alert.alert('Ошибка', 'Не удалось получить рассказ');
+  //   }
+  // };
 
   const [preferences, setPreferences] = useState('');
   const [location, setLocation] = useState('');
@@ -95,6 +105,22 @@ function HomeScreen(): React.JSX.Element {
       Alert.alert('Ошибка', 'Не удалось получить рассказ');
     }
   };
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const playPause = () => {
+    if (!isPlaying) {
+      handlePress();
+    }
+    setIsPlaying(prevState => !prevState);
+    setIsVisible(false);
+  };
+
+  const closeAction = () => {
+    setIsVisible(false);
+    setIsPlaying(false);
+  };
+
   return (
     <SafeAreaView style={styles.safeArea}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -102,7 +128,7 @@ function HomeScreen(): React.JSX.Element {
           <View style={styles.mapComponent}>
             <Map />
           </View>
-          <View style={styles.inputContainer}>
+          {/* <View style={styles.inputContainer}>
             <TextInput
               style={styles.input}
               placeholder="Введите предпочтения"
@@ -115,24 +141,82 @@ function HomeScreen(): React.JSX.Element {
               value={location}
               onChangeText={setLocation}
             />
-          </View>
+          </View> */}
           {isVisible && (
             <View style={styles.responseContainer}>
-              <TouchableOpacity style={styles.closeButton} onPress={() => setIsVisible(false)}>
+              <TouchableOpacity style={styles.closeButton} onPress={closeAction}>
                 <Text style={styles.closeButtonText}>✕</Text>
               </TouchableOpacity>
               <Text style={styles.responseText}>{responseText}</Text>
             </View>
           )}
-          <View style={styles.buttonContainer}>
-            <Pressable style={styles.button} onPress={handlePress}>
-              <Text style={styles.buttonText}>Начать рассказ</Text>
-            </Pressable>
-          </View>
-          <View style={styles.buttonTestContainer}>
-            <Pressable style={styles.buttonTest} onPress={handlePressTest}>
-              <Text style={styles.buttonTestText}>Тест</Text>
-            </Pressable>
+          <View style={styles.bottomContainer}>
+            <View style={styles.slideElement}>
+              <Line width={24} height={24} color={theme.colors.text} />
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.inputTop}
+                placeholder="Введите предпочтения"
+                value={preferences}
+                onChangeText={setPreferences}
+              />
+              <TextInput
+                style={styles.inputBot}
+                placeholder="Введите местоположение"
+                value={location}
+                onChangeText={setLocation}
+              />
+            </View>
+            <View style={styles.bottomSubContainer}>
+              <View style={styles.bottomSubContainerLeft}>
+                <Settings width={24} height={24} color={theme.colors.text} />
+              </View>
+              <View style={styles.bottomSubContainerCenter}>
+                <View>
+                {isPlaying ? (
+                  <Previous width={24} height={24} color={theme.colors.text} />
+                ) : (
+                  <Previous width={24} height={24} color={theme.colors.text2} />
+                )}
+                </View>
+                <View>
+                {isPlaying ? (
+                  <Minus15 width={24} height={24} color={theme.colors.text} />
+                ) : (
+                  <Minus15 width={24} height={24} color={theme.colors.text2} />
+                )}
+                </View>
+                <TouchableOpacity onPress={playPause}>
+                {isPlaying ? (
+                  <Pause width={24} height={24} color={theme.colors.text} />
+                ) : (
+                  <Play width={24} height={24} color={theme.colors.text} />
+                )}
+                </TouchableOpacity>
+                <View>
+                {isPlaying ? (
+                  <Plus15 width={24} height={24} color={theme.colors.text} />
+                ) : (
+                  <Plus15 width={24} height={24} color={theme.colors.text2} />
+                )}
+                </View>
+                <View>
+                {isPlaying ? (
+                  <Next width={24} height={24} color={theme.colors.text} />
+                ) : (
+                  <Next width={24} height={24} color={theme.colors.text2} />
+                )}
+                </View>
+              </View>
+              <View style={styles.bottomSubContainerRight}>
+              {isPlaying ? (
+                  <Like width={24} height={24} color={theme.colors.text} />
+                ) : (
+                  <Like width={24} height={24} color={theme.colors.text2} />
+                )}
+              </View>
+            </View>
           </View>
         </KeyboardAvoidingView>
       </TouchableWithoutFeedback>
@@ -143,6 +227,7 @@ function HomeScreen(): React.JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    fontFamily: theme.fonts.regular,
   },
   textStyle: {
     fontSize: 26,
@@ -153,21 +238,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFFFFF',
   },
   inputContainer: {
-    position: 'absolute',
+    // position: 'absolute',
     zIndex: 1,
-    bottom: 160,
-    left: 20,
-    right: 20,
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
-    padding: 10,
-    borderRadius: 10,
+    // bottom: 160,
+    // left: 20,
+    // right: 20,
+    // backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    // padding: 10,
+    marginBottom: 20,
+    gap: 10,
   },
-  input: {
+  inputTop: {
     borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 8,
+    borderColor: theme.colors.text2,
+    borderRadius: 10,
     padding: 10,
-    marginBottom: 10,
+    // marginBottom: 10,
+    backgroundColor: '#FFF',
+    fontSize: 16,
+  },
+  inputBot: {
+    borderWidth: 1,
+    borderColor: theme.colors.text2,
+    borderRadius: 10,
+    padding: 10,
     backgroundColor: '#FFF',
     fontSize: 16,
   },
@@ -243,6 +337,40 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
   },
+  bottomContainer: {
+    position: 'absolute',
+    zIndex: 100,
+    bottom: 0,
+    width: '100%',
+    paddingLeft: 16,
+    paddingRight: 16,
+    backgroundColor: theme.colors.background,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  slideElement: {
+    flex: 0,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    // marginBottom: 30,
+    marginBottom: 10,
+  },
+  bottomSubContainer: {
+    flex: 0,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 16,
+  },
+  bottomSubContainerLeft: {
+
+  },
+  bottomSubContainerCenter: {
+    flexDirection: 'row',
+    gap: 20,
+  },
+  bottomSubContainerRight: {
+
+  }
 });
 
 export default HomeScreen;
