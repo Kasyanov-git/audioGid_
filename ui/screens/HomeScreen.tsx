5cc2ab5197527837d8dc47668eebf007d0905f4c
vladimir <vbrezhnev@intetech.ru>
2025-03-05 17:38:17 +0300

    Фикс mergeConflict

--- ui/screens/HomeScreen.tsx
+++ ui/screens/HomeScreen.tsx
@@ -33,8 +33,12 @@ import { VolumeManager } from 'react-native-volume-manager';
 import Slider from '@react-native-community/slider';
 import LinearGradient from 'react-native-linear-gradient';
 import TrackPlayer, { State, useProgress, usePlaybackState } from 'react-native-track-player';
-
-const HomeScreen: React.FC = () => {
+import { Suggest  } from 'react-native-yamap';
+// import { GeoFigureType } from 'react-native-yamap/build/Search';
+import {ClassTimer} from './test.tsx';
+import { Geocoder } from 'react-native-yamap';
+Geocoder.init('500f7015-58c8-477a-aa0c-556ea02c2d9e');
+const HomeScreen: React.FC<{}> = () => {
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const [volume, setVolume] = useState<number>(0);
   const [containerHeight] = useState<number | 'auto'>('auto');
@@ -68,54 +72,13 @@ const HomeScreen: React.FC = () => {
 
       setAudioUrl(data.url);
       return data.url;
-import { Search,Suggest  } from 'react-native-yamap';
-import { GeoFigureType } from 'react-native-yamap/build/Search';
-import {ClassTimer} from './test.tsx';
-import { Geocoder } from 'react-native-yamap';
-Geocoder.init('500f7015-58c8-477a-aa0c-556ea02c2d9e');
-
-
-function HomeScreen(): React.JSX.Element {
-
-  const [responseText, setResponseText] = useState('');
-  // Тестовая функция для саджеста(выводит очень далеко, посмотреть настрйоку)
-  const find = async (query: string) => {
-    console.log(query);
-    const suggestions = await Suggest.suggest(query,{userPosition: {lat: 59.9537667, lon: 30.4121783}});
-    console.log(suggestions)
-    Suggest.reset();
-  }
-  function sayHello() {
-    console.log('Hello every 3 seconds');
-  }
-  const timer=new ClassTimer(5);
-  const handlePressTest = async () => {
-    // timer.start();
-    timer.fetchData();
-    try {
-      
-      
-      //Поиск по тексту(ищет как раз метки в близи)
-      // console.log("check");
-      // const searchResult = await Search.searchText(
-      //   'Парк',
-      //   { type: GeoFigureType.POINT, value: {lat: 59.9537667, lon: 30.4121783}},
-      //   { disableSpellingCorrection: true, geometry: true },
-      // );
-      // console.log("check1");
-      // console.log(searchResult);
-      
-      // const result= await Geocoder.geoToAddress({lat: 59.9537667, lon: 30.4121783});
-      //       console.log(result);
-      
-    } catch (error) {
-      console.error('Ошибка загрузки аудио:', error);
-      Alert.alert('Ошибка', 'Не удалось получить аудиофайл');
-      return null;
+    }catch (error) {
+        console.error('Ошибка загрузки аудио:', error);
+        Alert.alert('Ошибка', 'Не удалось получить аудиофайл');
+        return null;
+      }
     }
-  };
-
-  const playAudio = async (): Promise<void> => {
+      const playAudio = async (): Promise<void> => {
     if (isPlaying) return;
 
     let url = audioUrl;
@@ -246,55 +209,6 @@ function HomeScreen(): React.JSX.Element {
     setupVolumeManager();
   }, []);
 
-      console.error('Ошибка при запросе:', error);
-      Alert.alert('Ошибка', 'Не удалось получить рассказ');
-    }
-    
-  };
-  
-  const [preferences, setPreferences] = useState('');
-  const [location, setLocation] = useState('');
-  const [isVisible, setIsVisible] = useState(false);
-
-  
-  
-  
-  
-  const handlePress = async () => {
-    try {
-      timer.stop();
-      const response = await fetch('http://10.0.2.2:8000/ask', {  //ЗДЕСЬ МОЖНО ПОМЕНЯТЬ IP СЕРВЕРА
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        
-        body: JSON.stringify({
-          preferences,
-          location,
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP Error: ${response.status}`);
-      }
-
-      const data = await response.json();
-      setResponseText(data);
-  
-      console.log('Ответ от сервера:', data);
-
-      if (data.message) {
-        setResponseText(data.message);
-        setIsVisible(true);
-      } else {
-        throw new Error('Ответ не содержит поля "message"');
-      }
-    } catch (error) {
-      console.error('Ошибка при запросе:', error);
-      Alert.alert('Ошибка', 'Не удалось получить рассказ');
-    }
-  };
   return (
     <SafeAreaView style={styles.safeArea}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
